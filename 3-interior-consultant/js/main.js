48497cbadf760b26aecae75a89ab525394c24eea
abdoemr11 <abdoemr11@gmail.com>
2023-06-11 15:28:34 +0300

    fixed the issue temporily

--- 3-interior-consultant/js/main.js
+++ 3-interior-consultant/js/main.js
@@ -10,3 +10,13 @@ closeMenuBtn.onclick = () => {
     navMenu.style.display = 'none'
 
 }
+
+
+const mediaQuery = window.matchMedia('(min-width: 768px)')
+mediaQuery.addEventListener('change', (e) => {
+    if (e.matches) {
+        navMenu.style.display = 'block'
+        console.log('hi');
+    }
+})
+// Check if the media query is true
