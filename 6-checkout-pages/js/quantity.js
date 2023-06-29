2e06a60fb8a4a71a065d12991c8c63d5f02e8cc6
abdoemr11 <abdoemr11@gmail.com>
2023-06-29 10:43:46 +0300

    played with reef nearly finish

--- 6-checkout-pages/js/quantity.js
+++ 6-checkout-pages/js/quantity.js
@@ -1,29 +1,79 @@
-const decrementButtons = document.querySelectorAll('.quantity p:first-of-type')
-const incrementtButtons = document.querySelectorAll('.quantity p:last-of-type')
-console.log(decrementButtons);
-console.log(incrementtButtons);
+let { store, component } = reef;
+// Create a reactive data store
+let data = store({
+    countries: ['USA', 'Canada', 'UK', 'Germany', 'France', 'Australia'],
+    products: [
+        {
+            name: "Vintage Backpack",
+            price: 54.99,
+            discountedPrice: 94.99,
+            image: "images/photo1.png",
+            qunatity: 2
+        },
+        {
+            name: "Levi Shoes",
+            price: 74.99,
+            discountedPrice: 124.99,
+            image: "images/photo2.png",
+            qunatity: 1
 
-decrementButtons.forEach(decBtn => {
-    decBtn.addEventListener('click', () => {
-        const quantityLabel = decBtn.parentElement.querySelector('p:nth-of-type(2)')
+        },
+        {
+            name: "another thing",
+            price: 74.99,
+            discountedPrice: 124.99,
+            image: "images/photo2.png",
+            qunatity: 1
 
-        let newValue = Number((quantityLabel.textContent)) - 1
-        if (newValue < 0)
-            newValue = 0
-        quantityLabel.textContent = newValue
-        updateTotalLabel(priceValue * newValue)
-    })
-})
+        }
+    ]
 
-incrementtButtons.forEach(decBtn => {
-    decBtn.addEventListener('click', () => {
-        let quantityLabel = decBtn.parentElement.querySelector('p:nth-of-type(2)')
-        let newValue = Number((quantityLabel.textContent)) + 1
-        quantityLabel.textContent = newValue
-    })
-})
+});
 
-const updateTotalLabel = (newPrice) => {
-    const totalLabel = document.getElementById('total-price')
-    totalLabel.textContent = '$' + newPrice
+function CardTemplate() {
+    const productsHtml = data.products.map(product => `
+    <div class="card">
+      <img src="${product.image}" alt="">
+      <div class="info" onclick="testMe()">
+        <h3>${product.name}</h3>
+        <p class="price">
+          $${product.price.toFixed(2)} <span>$${product.discountedPrice.toFixed(2)}</span>
+        </p>
+        <div class="quantity">
+          <span onclick="decreaseQuantity(${data.products.indexOf(product)})" class="material-icons modifier">
+          remove
+          </span>
+          <p>${product.qunatity}</p>
+          <span  class="material-icons modifier" onclick>
+          add
+          </span>
+        </div>
+      </div>
+    </div>
+  `).join('');
+    const totalPrice = data.products.reduce((sum, product) => sum + product.qunatity * product.price, 0);
+
+    const summaryHtml = `  <div class="summarize">
+            <p><span>Shipping</span> <span>$19</span></p>
+            <p><span>Total</span> <span id="total-price">${totalPrice.toFixed(2)}</span></p>
+        
+        </div>`
+
+    return productsHtml.concat(summaryHtml)
 }
+component('.products', CardTemplate)
+
+//Create the countries
+function countriesTemplate() {
+    let countriesHtml = `
+    <option value="" hidden disabled selected>Your country..</option>
+
+    `
+    countriesHtml += data.countries.map(country => `
+    <option value="${country.toLowerCase()}" >${country}</option>
+
+    `)
+    return countriesHtml
+}
+
+component('#country', countriesTemplate)
