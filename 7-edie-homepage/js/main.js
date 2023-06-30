94931bf14a71992416b98ffd0c430dd121cc809b
abdoemr11 <abdoemr11@gmail.com>
2023-06-30 18:11:13 +0300

    finished image and scroll

--- 7-edie-homepage/js/main.js
+++ 7-edie-homepage/js/main.js
@@ -12,3 +12,43 @@ menuToggle.onclick = function () {
         menu_items.classList.add('sm-menu-hidden')
     }
 }
+
+
+// Get the image elements
+const images = document.querySelectorAll('.image-container img');
+
+// Set the desired percentage widths for different media queries
+const percentages = [
+    { mediaQuery: '(max-width: 480px)', width: '40%' },   // Example for max-width: 480px
+    { mediaQuery: '(max-width: 768px)', width: '70%' },   // Example for max-width: 768px
+    { mediaQuery: '(max-width: 992px)', width: '55%' },   // Example for max-width: 992px
+    { mediaQuery: '(max-width: 1200px)', width: '80%' }, // Examp
+    { mediaQuery: '(max-width: 10000px)', width: '100%' }, // Examp
+    // Add more media queries and desired widths as needed
+];
+// Calculate and set the width for each image based on the current media query
+function setWidths() {
+    const matchingPercentage = percentages.find(({ mediaQuery }) => matchMedia(mediaQuery).matches);
+    console.log(matchingPercentage);
+    console.log('try to changed to ', matchingPercentage.width);
+
+    if (matchingPercentage) {
+        const desiredWidth = matchingPercentage.width;
+        console.log('image changed to ', matchingPercentage.width)
+        images.forEach(image => {
+            const originalWidth = image.naturalWidth;
+            const calculatedWidth = `${(parseInt(desiredWidth) / 100) * originalWidth}px`;
+
+            image.style.width = calculatedWidth;
+        });
+    }
+}
+
+// Set the initial widths based on the current media query
+setWidths();
+
+// Listen for changes in the media query and update the widths accordingly
+percentages.forEach(({ mediaQuery }) => {
+    const media = matchMedia(mediaQuery);
+    media.addEventListener('change', setWidths);
+});
--- README.md
+++ README.md
@@ -27,3 +27,4 @@
 ## 7 Edie homepage
 - I saw a wise reason for not putting margin or padding on main body.
 - In the ourteam section when using grid I was getting all image with same width which wasn't in the design. The solution is simple don't set max-width to 100% leave it to default. It result in horizontal mobil scroll so the problem isn't solved till now.
+- The solution to this dilemma is to use javascript. With matchMedia and naturalWidth It can achive the trick easiy. 
